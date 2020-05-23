1a7d678926b7b63b2fe668d17f042d7cf203b99a
harroshan <harroshanmakkar04@gmail.com>
2020-05-23 13:44:27 +0530

    Authentication

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,23 +7,15 @@ export const config = {
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
+    "jwt_secret": process.env.JWT_SECRET
   },
-  // "dev": {
-  //   "username": "hariproject",
-  //   "password": "hari2504",
-  //   "database": "udagramhari",
-  //   "host": "hariproject.caoisjepsdp7.ap-south-1.rds.amazonaws.com",
-  //   "dialect": "postgres",
-  //   "aws_region": "ap-south-1",
-  //   "aws_profile": "default",
-  //   "aws_media_bucket": "udagramhari"
-  // },
   "prod": {
     "username": process.env.PG_USERNAME,
     "password": process.env.PG_PASSWORD,
     "database": process.env.PG_DATABASE,
     "host": process.env.PG_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "jwt_secret": process.env.JWT_SECRET
   }
 }
